b71def0df7743e0c3062cc368a52bcabb3b763d6
SweetArctic <juanfer46934@gmail.com>
2023-08-17 22:10:37 -0500

    uccworks

---
+++ javascript/scroll.js
@@ -0,0 +1,12 @@
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector("header").style.top = "0";
+//   } else {
+//     document.querySelector("header").style.top = "-60px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+
+window.alert="hola mundo";
